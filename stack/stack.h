1da27776b6266cfdc182589d3853477cffedf383
Ricardo Alejos <ricardo.alejos@iceye.fi>
2019-06-19 09:00:09 +0300

    Adds license information headers to source files.

--- stack/stack.h
+++ stack/stack.h
@@ -1,9 +1,20 @@
-/*
- * stack.h
- *
- *  Created on: Apr 17, 2019
- *      Author: ricardo
- */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * This file is part of the Data Structures For Embedded Systems (DSFES).     *
+ * Copyright (c) 2019 Ricardo Alejos <ricardoalejos20@gmail.com>              *
+ *                                                                            *
+ * DSFES is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * DSFES is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with DSFES.  If not, see <https://www.gnu.org/licenses/>.            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef DATA_STRUCTURES_FOR_EMBEDDED_SYSTEMS_STACK_STACK_H_
 #define DATA_STRUCTURES_FOR_EMBEDDED_SYSTEMS_STACK_STACK_H_
